abc24e58e7e905d47b5a4417ccedb6cbfab35795
Rob Neville <rn@retailarchitects.com>
2021-09-10 19:44:41 -0400

    34

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,19 @@ private
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
+    def require_admin
+        unless current_user_admin?
+            redirect_to root_url, alert: "Unauthroized access!"
+        end
+    end
+
+    def current_user_admin?
+        current_user && current_user.admin?
+    end
+
     helper_method :current_user
 
     helper_method :current_user?
+
+    helper_method :current_user_admin?
 end
